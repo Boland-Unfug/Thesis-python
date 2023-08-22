87d090564a3cc829236143ccb585574ff4d9cc9d
Boland Unfug <booglebeaste@gmail.com>
2023-08-21 19:21:09 -0600

    First look at thesis idea, setting up the basics.

---
+++ agent.py
@@ -0,0 +1,63 @@
+# Description: This file contains the Agent class and its subclasses.
+import abc
+# The Agent class is the parent class for all agents. it contains the score.
+
+
+class Agent():
+    """
+    The Agent class is the parent class for all agents. 
+    it contains the score, the memory of the agent, and the memory of the opponent, but cannot be instantiated.
+    it also includes several methods:
+    get_score() returns the score of the agent
+    change_score() changes the score of the agent
+    set_score() sets the score of the agent
+    play() returns the move of the agent
+    memorize() adds the move of the agent to the memory
+    memorize_opponent() adds the move of the opponent to the memory
+    forget_move() removes the last move of the agent from the memory
+    forget_opponent_move() removes the last move of the opponent from the memory
+    forget() resets the memory of the agent
+    get_memory() returns the memory of the agent
+    get_opponent_memory() returns the memory of the opponent
+    """
+
+    score = 0
+    self_memory = []
+    opponent_memory = []
+
+    
+    def __init__(self, opponent_memory, self_memory, score):
+        pass
+
+    def get_score(self):
+        pass
+
+    def change_score(self, score):
+        pass
+
+    def set_score(self, score):
+        pass
+
+    def play(self):
+        pass
+
+    def memorize(self, state):
+        pass
+
+    def memorize_opponent(self, state):
+        pass
+
+    def forget_move(self):
+        pass
+
+    def forget_opponent_move(self):
+        pass
+
+    def forget(self):
+        pass
+
+    def get_memory(self):
+        pass
+
+    def get_opponent_memory(self):
+        pass
